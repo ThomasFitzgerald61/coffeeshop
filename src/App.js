b39c116e6c42239e587f5c3214e2b93e3afacc2b
Thomas Fitzgerald <thomasfitzgerald61@gmail.com>
2022-01-08 20:57:23 +1100

    time to start

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Hello World.
+          Edit <code>src/App.js</code> This is the start to my coffee shop app.
         </p>
         <a
           className="App-link"
