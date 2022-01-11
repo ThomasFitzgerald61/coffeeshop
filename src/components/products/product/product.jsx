33f5e417958de4e1d4b5161d5cfd806add02d432
Thomas Fitzgerald <thomasfitzgerald61@gmail.com>
2022-01-11 13:52:24 +1100

    start build

---
+++ src/components/products/product/product.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, CardBotton } from '@material-ui/core';
+import { addShoppingCart } from '@material-ui/icons'
+import { mergeClasses } from '@material-ui/styles';
+
+const product = () => {
+    return (
+        <Card className={Classes.root}>
+            <CardMedia />
+        </Card>
+    )
+}
+
+export default product
---
+++ src/components/products/products.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+const products [
+    {id: 1, name: 'Dukes', description: 'Espresso' }
+    {id: 2, name: 'Supreme', description: 'Espresso' }
+    {id: 3, name: 'Axel', description: 'Espresso' }
+]
+
+const Products = () => {
+    <main>
+        <Grid container justify='center' spacing={4}>
+            {products.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product />
+                </Grid>
+            ))}
+        </Grid>
+    </main>
+}
+
+export default Products; 
